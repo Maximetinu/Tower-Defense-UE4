cec698776356d966b5bc072f6419379002026af6
Maximetinu <maximetinu@gmail.com>
2018-01-15 07:46:39 +0100

    delete old

--- Source/TowerDefenseUE4/TowerDefenseUE4.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "TowerDefenseUE4.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TowerDefenseUE4, "TowerDefenseUE4" );
